bc2ad65fde8ad1b1de1a496421eeec7ff8d7c3d1
Ismael Rodrigo <ismaelbrasil02@gmail.com>
2023-07-12 14:03:10 -0300

    Melhoria dos testes

--- src/agendamento.test.ts
+++ src/agendamento.test.ts
@@ -1,15 +1,16 @@
 import { assertEquals , assertNotEquals } from "https://deno.land/std@0.193.0/testing/asserts.ts";
-import axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
+import { CriarAgendamento } from "./caso-de-uso/criar-agendamento.ts";
+import { AgendamentoEmMemoriaImpl } from "./implementacoes/agendamento-em-memoria.ts";
 
-Deno.test("Deve criar agendamento caso os dados sejam válidos", async () => {
-    const response = await axiod.post("http://localhost:8000/agendamento", {
+Deno.test("Deve criar agendamento caso os dados sejam válidos", () => {
+    const agendamentoRepository = new AgendamentoEmMemoriaImpl([]);
+    const criarAgendamento = new CriarAgendamento(agendamentoRepository);
+    const agendamento = criarAgendamento.executar({
             nome: "João",
             idade: 20,
             data_agendamento: "2021-10-10",
         },
     );
-    const agendamento = await response.data;
-    assertEquals(response.status, 201);
     assertNotEquals(agendamento.id, undefined);
     assertEquals(agendamento.nome, "João");
     assertEquals(agendamento.idade, 20);
